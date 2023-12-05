b5697edce3d683e74b3bfdc2aed54cba064d3d20
danil <dddd@gmail.com>
2023-12-05 13:51:08 +0300

    Добавьте файлы проекта.

---
+++ antonov227/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace antonov227
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(TbNumberA.Text);
+            double b = Convert.ToDouble(TbNumberB.Text);
+            double s = a * b;
+            double p = 2* (a+b);
+            TextBlockAnswer.Text = $"Ответ:\n площадь = {s}\n периметр = {p}";
+        }
+    }
+}
